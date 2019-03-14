d5fe3e06f032d12a1e1a3ff013f987946693bb49
Nirossan <46507846+Nirossan@users.noreply.github.com>
2019-03-14 18:02:15 -0400

    Add files via upload

--- topcreators/assets/javascript/app.js
+++ topcreators/assets/javascript/app.js
@@ -4,77 +4,55 @@ var link ="";
 var resultId="";
 var title ="";
 
-    var newsURL="https://newsapi.org/v2/everything?sources=entertainment-weekly&apiKey=cfa7c039edef460a8f6723558e31f499";
-    var topNewsURL="https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=cfa7c039edef460a8f6723558e31f499";
-    var googleTopNews="https://newsapi.org/v2/top-headlines?country=ca&apiKey=cfa7c039edef460a8f6723558e31f499";
-    var searchTermNews="https://newsapi.org/v2/everything?q="+title+"&language=en&sortBy=relevancy&apiKey=7a70b0d355fc47d4a73c34f23bbb6db5";
-
 $(document).ready(function(){
+        $(".movieNewsContainer").hide();
 
-         var images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg'];
-    
+         var images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+         var footerImages = ['11.jpg', '12.png', '13.jpg', '14.jpg', '15.jpg'];
          $('#header').css({'background-image': 'url(assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
-         $('#footer').css({'background-image': 'url(assets/imagesniro/' + images[Math.floor(Math.random() * images.length)] + ')'});
-        addNews(newsURL,".topNews");
-
+         $('#footer').css({'background-image': 'url(assets/images/' + footerImages[Math.floor(Math.random() * footerImages.length)] + ')'});
+        addNews();
+        //The function below adds Top 10 now playing movies right now
         $("#nowPlaying").on("click",function(event){
             event.preventDefault();
            $(".buttonBox").empty();
            $(".movieInfo").empty();
            
-            // Here we are building the URL we need to query the database
            var queryURL = "https://api.themoviedb.org/3/movie/now_playing?api_key="+APIKey+"&language=en-US&page=1";
           
-            // Here we run our AJAX call to the OpenWeatherMap API
             $.ajax({
             url: queryURL,
             method: "GET"
             })
-            // We store all of the retrieved data inside of an object called "response"
             .then(function(response) {
-                //console.log(response);
-                // Log the resulting object
-                //console.log(response.results[0].original_language);
                 addButtonsWithMovieInfo(response);
             });
         });
-
-            $(document).on("click",".nowPlayingButton",function(event){
-               // console.log(event);
-                playYoutube(event);
-                $(".topNews").empty();
-              // var movieNewsDiv =$("<div>");
-               title = event.currentTarget.value.toString().split(' ').join('+').trim();
-               searchTermNews="https://newsapi.org/v2/everything?q="+title+"&language=en&sortBy=relevancy&apiKey=7a70b0d355fc47d4a73c34f23bbb6db5";
-              // var whichButton= event.currentTarget.id.substring(10);
-              // var div =".movieInfo"+whichButton;
-               //console.log(title);
-               addNews(searchTermNews,".topNews")
-            //    $(".movieInfo").append(movieNewsDiv);
-            //    movieNewsDiv.text(addNews(searchTermNews,".movieInfo"));
-            //      movieNewsDiv.addClass("movieNews");
-                
-            });
+        //The function below plays youtube video of clicked movie and adds news of that movie below
+        $(document).on("click",".nowPlayingButton",function(event){
+             playYoutube(event);
+             $(".movieNewsContainer").show();
+             addQueryNews(event.toElement.innerHTML);    
+         });
         
 
-
+         //The function below adds max 10 buttons of movie searched in input bar 
         $("#searchButton").on("click",function(event){
             event.preventDefault();
             $(".movieInfo").empty();
             $(".buttonBox").empty();
+            $(".movieNewsContainer").show();
 
             var title = $("#inputMovie").val();
-           
+            addQueryNews(title);
             var queryURL = "https://api.themoviedb.org/3/search/movie?api_key="+APIKey+"&language=en-US&query="+title+"&page=1&include_adult=false";
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
                 })
-                //We store all of the retrieved data inside of an object called "response"
                 .then(function(response) {
-                    //Log the resulting object
-                    //console.log(response.results[0].original_language);
+                    
                     addButtonsWithMovieInfo(response);
     
                 });
@@ -84,23 +62,56 @@ $(document).ready(function(){
         
         
 
-       
+        // The function below until addNews() adds chat box 
+            var config = {
+                apiKey: "AIzaSyAQJ_F-7GSyLAbPYlgoVPaX15W_WJPYkcw",
+                authDomain: "topcreatorsproject1.firebaseapp.com",
+                databaseURL: "https://topcreatorsproject1.firebaseio.com",
+                projectId: "topcreatorsproject1",
+                storageBucket: "topcreatorsproject1.appspot.com",
+                messagingSenderId: "874107222924"
+            };
+            firebase.initializeApp(config);
 
+
+            var database = firebase.database();
+            var message = "";
+
+            $("#submitButton").on("click", function (event) {
+                event.preventDefault();
+                message = $("#inputText").val().trim();
+                database.ref().set({
+                message: message,
+                });
+            });
+
+
+            database.ref().on("value", function (snapshot) {
+                //console.log(snapshot.val());
+                //console.log(snapshot.val().message);
+                database.ref().set(snapshot.val().message);
+                $("#messageArea").prepend("<br>" + snapshot.val().message + "</br>");
+            }, function (errorObject) {
+                //console.log("The read failed: " + errorObject.code);
+            });
+       
 
     
 });
 
-
-function addNews(news,whichDiv)
+//This function will add 20 ongoing news about tvshows and movies
+function addNews()
 {
-   
+    var newsURL="https://newsapi.org/v2/everything?sources=entertainment-weekly&apiKey=cfa7c039edef460a8f6723558e31f499";
+    var topNewsURL="https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=cfa7c039edef460a8f6723558e31f499";
+    var googleTopNews="https://newsapi.org/v2/top-headlines?country=ca&apiKey=cfa7c039edef460a8f6723558e31f499";
+
     $.ajax({
-        url: news,
+        url: newsURL,
         method: "GET"
         })
-        // We store all of the retrieved data inside of an object called "response"
         .then(function(response) {
-           // console.log(response);
+            //console.log(response);
             
             
             for(var i=0;i<20;i++)
@@ -111,44 +122,88 @@ function addNews(news,whichDiv)
                 var newsImage=$("<img>");
                 var publish =$("<div>");
 
-                $(whichDiv).append(publish);
+                $(".topNews").append(publish);
                 var newsDate=response.articles[i].publishedAt;
                 var momentDate = moment(newsDate,'YYYY-MM-DD');
                 publish.text(momentDate.format('LL'));
                 publish.addClass("newsDate");
                 
 
-                $(whichDiv).append(newsDiv);
+                $(".topNews").append(newsDiv);
                 newsDiv.append(title);
                 newsDiv.addClass("newsDivTitle");
                 title.text(i+1+". "+response.articles[i].title);
-                title.attr("href",response.articles[i].url);
-               
+                title.attr("href",response.articles[i].url); 
                 
-                $(whichDiv).append(newsImage);
+                $(".topNews").append(newsImage);
                 newsImage.addClass("newsImage");
                 newsImage.attr("src",response.articles[i].urlToImage);
 
-                $(whichDiv).append(content);
+                $(".topNews").append(content);
                 content.text(response.articles[i].content);
                 content.addClass("newsDivContent");
-
-                sticky();
-                
-
 
             }
 
         });
 }
 
+//This function will add 20 movie news
+function addQueryNews(title)
+{
+    $(".movieNews").empty();
+    var queryNews="https://newsapi.org/v2/everything?q="+title+"&language=en&sortBy=relevancy&apiKey=7a70b0d355fc47d4a73c34f23bbb6db5";
 
+    $.ajax({
+        url: queryNews,
+        method: "GET"
+        })
+        .then(function(response) {
+            //console.log(response);
+            
+            for(var i=19;i>=0;i--)
+            {
+                var newsDiv =$("<div>");
+                var title =$("<a>");
+                var content = $("<div>");
+                var newsImage=$("<img>");
+                var publish =$("<div>");
+                var containerDiv=$("<div>");
+
+                $(containerDiv).prepend(content);
+                content.text(response.articles[i].content);
+                content.addClass("movieNewsContent");
+
+                $(containerDiv).prepend(newsDiv);
+                newsDiv.append(title);
+                newsDiv.addClass("movieNewsTitle");
+                title.text(i+1+". "+response.articles[i].title);
+                title.attr("href",response.articles[i].url);
+
+                $(containerDiv).prepend(publish);
+                var newsDate=response.articles[i].publishedAt;
+                var momentDate = moment(newsDate,'YYYY-MM-DD');
+                publish.text(momentDate.format('LL'));
+                publish.addClass("movieNewsDate");
+
+                $(".movieNews").prepend(containerDiv);
+                containerDiv.addClass("col-md-8");
+
+                $(".movieNews").prepend(newsImage);
+                newsImage.addClass("movieNewsImage");
+                newsImage.attr("src",response.articles[i].urlToImage);
+                newsImage.addClass("col-md-4");
+
+            }
+
+        });
+    }
+
+//This function will search for trailer on youtube
 function searchYoutube(search)
 {
-   //var resultId="";
    var APIKey = "AIzaSyBtp6rXtBjreYY-3-sZ3LntzY6ptXDnhA8";
    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q="+search+"+trailer&type=video&key=" + APIKey;
-  // console.log(queryURL);
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -161,7 +216,7 @@ function searchYoutube(search)
 
 }
 
-
+//This function will play youtube video
 function playYoutube(event) {
 
     var target = event.currentTarget.id;
@@ -180,10 +235,8 @@ function playYoutube(event) {
         if(response.results.length===0)
         {
              searchYoutube(movieName);
-            //console.log(resultId);
              link= "https://www.youtube.com/embed/"+resultId+"?autoplay=1";
              $("#youtube").attr("src",link);
-             //console.log("from youtube API");
         }else{
             youtubeId = response.results[0].key; 
              link = "https://www.youtube.com/embed/"+youtubeId+"?autoplay=1";
@@ -200,7 +253,7 @@ function playYoutube(event) {
 }
 
 
-
+//This function will add movie buttons from TMDB with its info
 function addButtonsWithMovieInfo(response) {
 
     var count =0;
@@ -251,25 +304,11 @@ function addButtonsWithMovieInfo(response) {
 }
 
 
-function sticky() {
-    if ($(".topNews").pageYOffset > sticky || $(".youtubeBox").pageYOffset > sticky ||$(".buttonBox").pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+// function sticky() {
+//     if ($(".topNews").pageYOffset > sticky || $(".youtubeBox").pageYOffset > sticky ||$(".buttonBox").pageYOffset > sticky) {
+//       header.classList.add("sticky");
+//     } else {
+//       header.classList.remove("sticky");
+//     }
+//   }
 
-
-/* <script src="https://www.gstatic.com/firebasejs/5.8.6/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAjOxb1aI7qoKCf---a-nkywc2oOXxov44",
-    authDomain: "movieshuffle-576b5.firebaseapp.com",
-    databaseURL: "https://movieshuffle-576b5.firebaseio.com",
-    projectId: "movieshuffle-576b5",
-    storageBucket: "movieshuffle-576b5.appspot.com",
-    messagingSenderId: "358540179940"
-  };
-  firebase.initializeApp(config);
-</script> */
